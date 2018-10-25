083a046ec7964bde47907ca3a87a708195a1d769
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-10-25 13:34:13 -0400

    Added CSS and Improved Info Display
     - Added the style.css file in the styles folder
     - Created a functions in Main.js to improve the display of info

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -14,6 +14,13 @@ function getWeather () {
 
     if (request.status >= 200 && request.status < 400) {
 
+      const app = document.getElementById('root');
+
+      const container = document.createElement('div');
+      container.setAttribute('class', 'container');
+
+      app.appendChild(container);
+
       var data = JSON.parse(this.response);
 
       var weatherData = data.weather[0];
@@ -21,14 +28,32 @@ function getWeather () {
 
       console.log(mainData);
       console.log(weatherData);
-      // weatherJSON = JSON.stringify(weatherData);
 
-      // console.log(weatherData);
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
 
-      document.getElementById('weather').innerHTML = weatherData.main;
-      
-      document.getElementById('cel').innerHTML = tempConvK(0, mainData.temp);
-      document.getElementById('far').innerHTML = tempConvK(1, mainData.temp);
+      const icon = document.createElement('img');
+      icon.src =  buildIconPath(weatherData.icon);
+
+      const weatherMain = document.createElement('h1');
+      weatherMain.textContent = weatherData.main;
+
+      const tempC = document.createElement('p');
+      const tempF = document.createElement('p');
+
+      tempC.textContent = 'Temperature (Celsius): ' + tempConvK(0, mainData.temp);
+      tempF.textContent = 'Temperature (Farenheit): ' + tempConvK(1, mainData.temp);
+
+      container.appendChild(card);
+
+      card.appendChild(weatherMain);
+      card.appendChild(icon);
+      card.appendChild(tempC);
+      card.appendChild(tempF);
+      // document.getElementById('weather').innerHTML = weatherData.main;
+      //
+      // document.getElementById('cel').innerHTML = 'Temperature (Celsius): ' + tempConvK(0, mainData.temp);
+      // document.getElementById('far').innerHTML = 'Temperature (Farenheit): ' + tempConvK(1, mainData.temp);
 
     } else {
       console.log();
@@ -40,15 +65,43 @@ function getWeather () {
  request.send();
 }
 
+function getRanCaliCity () {
+  // Returns the name of a random California city (because California is the coolest state)
+  var ran = Math.floor((Math.random() * 6));
+
+  if (ran === 0) {
+      document.getElementById('mainForm').city.value = "San Francisco";
+  } else if (ran === 1) {
+      document.getElementById('mainForm').city.value = "Los Angeles";
+  } else if (ran === 2) {
+      document.getElementById('mainForm').city.Value = "Pasadena";
+  } else if (ran === 3) {
+      document.getElementById('mainForm').city.value = "Mountain View";
+  } else if (ran === 4) {
+      document.getElementById('mainForm').city.value = "San Jose";
+  } else if (ran === 5) {
+      document.getElementById('mainForm').city.value = "Palo Alto";
+  } else if (ran === 6) {
+      document.getElementById('mainForm').city.value = "Berkeley";
+  } else {
+      document.getElementById('mainForm').city.value = "Cupterino";
+  }
+
+}
+
 function tempConvK (caseN, temp) {
   // Converts from kelvin to celsius and farenheit depending on the case number value
   if (caseN === 0) {
       // Kelvin to Celsius
-      return temp - 273.15;
+      return Math.round(temp - 273.15);
   } else {
       // Otherwise, convert to farehnheit
-      return (temp - 273.15) * (9/5) + 32;
+      return Math.round((temp - 273.15) * (9/5) + 32);
   }
+}
+
+function buildIconPath (icon) {
+  return 'http://openweathermap.org/img/w/' + icon + '.png';
 }
 
 function buildPathCity (city) {
